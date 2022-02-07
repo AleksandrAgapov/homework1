20cc27541c458c9c8730b7a5d49ebbcf5156de42
Alexander <alek.agapow80@yandex.ru>
2022-02-07 20:25:33 +0700

    задача вып

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+	// По двум заданным числам проверять является ли первое квадратом второго
+
+
+int a=36;
+int b=6;
+// int square = b*b;
+
+int square = (int)Math.Pow(b,2);
+
+if (square==a)
+
+  Console.WriteLine(" a  является квадратом b");
+
+  else Console.WriteLine( " a не является квадратом b");
